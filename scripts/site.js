7066c3625159ee2228fc11dfa152d2860db460f9
marisgarrett <35425228+marisgarrett@users.noreply.github.com>
2018-01-14 14:37:33 +0000

    Initial work

---
+++ scripts/site.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    var navbar = document.getElementsByClassName("navbar-fixed")[0];
+
+    // if (navbar.classList.contains("active")) {
+    //     navbar.classList.remove("active");
+    // }
+    // else {
+        navbar.classList.toggle("active");
+    //}
+}
